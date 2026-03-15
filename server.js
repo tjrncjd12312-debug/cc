33b5f71e413298af92268d0d395066426b68d037
goddns12312 <goddns12312@gmail.com>
2026-03-16 01:17:57 +0900

    railway port setting

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.use('/user',   express.static(path.join(__dirname, 'user')));
 const txCollector = require('./lib/transactionCollector');
 txCollector.start();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
